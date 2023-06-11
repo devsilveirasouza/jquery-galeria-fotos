3c977c04f9e9b17b75e20e4f1aa2121ad95311b0
devsilveirasouza <dev.silveirasouza@gmail.com>
2023-06-11 15:16:52 -0300

    fix: adicionado o envio de imagens e efeitos

--- main.js
+++ main.js
@@ -10,4 +10,24 @@ $(document).ready(function() {
         // Recolhe o formulário
         $('form').slideUp();
     }) 
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        /** Aplica o display none inline ao adicionar a nova imagem 
+         * para executar o efeito de fadeIn */
+        const novoItem = $('<li style="display: none"></li>');
+        /** Adiciona o enderecoDaNovaImagem ao novo item */
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+            `).appendTo(novoItem);
+            $(novoItem).appendTo('ul');
+            $(novoItem).fadeIn(1000); /* Efeito ao adicionar uma nova imagem */
+            $('#endereco-imagem-nova').val('');
+    })    
 })
